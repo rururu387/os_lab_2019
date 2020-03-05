67e38fc8692fdae32093a487bf061f75870152f1
rururu387 <rururu387@gmail.com>
2020-03-05 11:01:29 +0000

    Lab2 half done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,16 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = 0;
+    while(str[length] != '\0' && str[length] != '\n')
+    {
+        length++;
+    }
+    for (int cnt = 0; cnt < length / 2; cnt++)
+    {
+        char t = str[cnt];
+        str[cnt] = str[length - cnt - 1];
+        str[length - cnt - 1] = t;
+    }
 }
 
